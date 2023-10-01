1efddcb42f0fde2864600838102652cf8f11610d
Omar Yahya <omar.yahya@kcl.ac.uk>
2023-10-02 00:06:49 +0100

    Exercise solved

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if not(str(item) in frequencies):
+            frequencies.update({str(item) : 1})
+        else:
+            oldValue = frequencies.get(str(item))
+            oldValue += 1
+            frequencies.update({str(item) : oldValue})
+
     return frequencies
